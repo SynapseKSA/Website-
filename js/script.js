33d356785007872cd1faadeea16018e29cf9ead4
SynapseKSA <SynapseAdmin@synapseksa.com>
2025-01-06 09:32:20 +0300

    add renad part

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Website is loaded and ready!");
+const counts = document.querySelectorAll(".count");
+const speed = 10;
+
+counts.forEach((counter) => {
+  function upDate() {
+    const target = Number(counter.getAttribute("data-target"));
+    const count = Number(counter.innerText);
+    const inc = target / speed;
+    if (count < target) {
+      counter.innerText = Math.floor(inc + count);
+      setTimeout(upDate, 30);
+    } else {
+      counter.innerText = target;
+    }
+  }
+  upDate();
 });
